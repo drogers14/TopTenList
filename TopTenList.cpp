7d317aa34ec80945d49e6d9c8b67d05fc54c97ac
Destiny Rogers <destinyrogers725@gmail.com>
2020-04-01 11:32:54 -0700

    update week9

--- TopTenList.cpp
+++ TopTenList.cpp
@@ -7,15 +7,24 @@ using std::string;
 
 TopTenList::TopTenList()
 {
-
+  _list.resize(10);
 }
 
 void TopTenList::set_at(int index, Hyperlink link)
 {
+  if(index < 1 || index > _list.size()){
+    return;
+  }
+
+  _list[index-1] = link;
+  return;
 
 }
 
 Hyperlink TopTenList::get(int index)
 {
-
+  if(index < 1 || index > _list.size()){
+    exit(1);
+  }
+  return _list[index-1];
 }
--- main.cpp
+++ main.cpp
@@ -14,5 +14,7 @@ int main()
     cpp.url = "https://www.cplusplus.com/";
     languages->set_at(1, cpp);
 
+//    languages->resize(100);
+
     return 0;
 }
