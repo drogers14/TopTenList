9fe6517cb8627109a1ff9fbfdc1a0829b9ed3104
Destiny Rogers <destinyrogers725@gmail.com>
2020-04-01 14:54:56 -0700

    finish x12

---
+++ IDirectionable.h
@@ -0,0 +1,11 @@
+#ifndef IDIRECTIONABLE_H
+#define IDIRECTIONABLE_H
+
+class IDirectionable {
+  public:
+
+    virtual void display_forward() = 0;
+    virtual void display_backward() = 0;
+};
+
+#endif
--- TopTenList.cpp
+++ TopTenList.cpp
@@ -1,9 +1,12 @@
 //Manages a list of top ten hyperlinks
 #include <string>
+#include <iostream>
 #include "TopTenList.h"
 #include "Hyperlink.h"
 
 using std::string;
+using std::cout;
+using std::endl;
 
 TopTenList::TopTenList()
 {
@@ -27,4 +30,21 @@ Hyperlink TopTenList::get(int index)
     exit(1);
   }
   return _list[index-1];
+}
+
+void TopTenList::display_forward(){
+  for(int i=0; i<_list.size(); i++){
+    cout<<i+1<<". <a href="<<'"';
+    cout<<_list[i].url;
+    cout<<'"'<<'>'<<_list[i].text<<"</a>"<<endl;
+  }
+}
+
+void TopTenList::display_backward(){
+  for(int i=_list.size()-1; i>=0; i--){
+    cout<<i+1<<". <a href="<<'"';
+    cout<<_list[i].url;
+    cout<<'"'<<'>'<<_list[i].text<<"</a>"<<endl;
+  }
+
 }
